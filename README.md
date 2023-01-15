81543705e588a8c7f94c8af7b4ae2630a247ef65
Simonexc <szymekdt@gmail.com>
2023-01-15 16:52:17 +0100

    Clone repos

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Autoclone
 
 ## Run autoclone
-To download and run autoclone server at your machine you need to add `config.json`,
+To download and run autoclone server at your machine you need to add `config.json` (add `/` at the end of folder path),
 download the executable file `rm -f Autoclone && wget https://github.com/GestureToSpeech/Autoclone/raw/master/bin/Autoclone -O Autoclone && sudo chmod -R 0777 Autoclone`
 and run it `rm -f nohup.out && nohup ./Autoclone &`. This will start the server in the background. Logs will be in
 `nohup.out`. To stop it, run `kill $(pgrep Autoclone)`.
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type Config struct {
@@ -21,7 +23,7 @@ func main() {
 	// Open the file
 	data, err := ioutil.ReadFile("config.json")
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		log.Printf("Error reading file: %s", err)
 		return
 	}
 
@@ -29,41 +31,51 @@ func main() {
 	var config Config
 	err = json.Unmarshal(data, &config)
 	if err != nil {
-		fmt.Println("Error parsing JSON:", err)
+		log.Printf("Error parsing JSON: %s", err)
 		return
 	}
 
-	// Print the values of the MainFolder and Repos fields
-	fmt.Println("Push folder:", config.PushFolder)
-	fmt.Println("Pull folder:", config.PullFolder)
-	fmt.Println("Repos:")
-	for _, repo := range config.Repos {
-		fmt.Println("  SSH:", repo.Ssh)
-		fmt.Println("  Key:", repo.Key)
-	}
-
-	// remove pull and push folders
-	err = executeCommand("", "rm", "-rf", fmt.Sprintf("\"%s\"", config.PushFolder))
-	if err != nil {
-		fmt.Println("Error removing push folder", err)
-		return
-	}
-	err = executeCommand("", "rm", "-rf", fmt.Sprintf("\"%s\"", config.PullFolder))
-	if err != nil {
-		fmt.Println("Error removing pull folder", err)
-		return
-	}
-
+	/*
+		// remove pull and push folders
+		err = executeCommand("", "rm", "-rf", fmt.Sprintf("\"%s\"", config.PushFolder))
+		if err != nil {
+			log.Printf("Error removing push folder %s", err)
+			return
+		}
+		err = executeCommand("", "rm", "-rf", fmt.Sprintf("\"%s\"", config.PullFolder))
+		if err != nil {
+			log.Printf("Error removing pull folder %s", err)
+			return
+		}
+	*/
 	// create pull and push folders
 	err = executeCommand("", "mkdir", fmt.Sprintf("\"%s\"", config.PushFolder))
 	if err != nil {
-		fmt.Println("Error creating push folder", err)
+		log.Printf("Error creating push folder %s", err)
 		return
 	}
 	err = executeCommand("", "mkdir", fmt.Sprintf("\"%s\"", config.PullFolder))
 	if err != nil {
-		fmt.Println("Error creating pull folder", err)
+		log.Printf("Error creating pull folder %s", err)
 		return
+	}
+
+	// clone repos
+	for _, repo := range config.Repos {
+		log.Printf("Cloning repo %s", repo.Ssh)
+		err = cloneRepo(config.PullFolder, repo.Ssh)
+		if err != nil {
+			log.Printf("Error cloning repo %s; error message: %s", repo.Ssh, err)
+			return
+		}
+
+		allBranches, err := getAllBranches(config.PullFolder, repo.Ssh)
+		if err != nil {
+			log.Printf("Couldn't get all branches from repo %s; error message: %s", repo.Ssh, err)
+			return
+		}
+
+		log.Print(allBranches)
 	}
 }
 
@@ -78,4 +90,90 @@ func executeCommand(dir string, commandName string, arg ...string) error {
 		return err
 	}
 	return nil
+}
+
+func getRepoFolder(ssh string, folder string) string {
+	repoSSHParts := strings.Split(ssh, "/")
+	repoName := repoSSHParts[len(repoSSHParts)-1]
+	repoName = strings.TrimSuffix(repoName, ".git")
+
+	return folder + repoName + "/"
+}
+
+func copyFiles(pullDir string, pushDir string, repoSSH string) error {
+	destDir := getRepoFolder(repoSSH, pushDir)
+	originDir := getRepoFolder(repoSSH, pullDir)
+
+	// remove old files
+	err := executeCommand(
+		"",
+		"[",
+		"-d",
+		fmt.Sprintf("\"%s\"", destDir),
+		"]",
+		"&&",
+		"find",
+		fmt.Sprintf("\"%s\"", destDir),
+		"-mindepth",
+		"1",
+		"-not",
+		"-path",
+		fmt.Sprintf("\"%s/.git/*\"", destDir),
+		"-not",
+		"-path",
+		fmt.Sprintf("\"%s/.git\"", destDir),
+		"-delete",
+	)
+	if err != nil {
+		return err
+	}
+
+	// copy files
+	err = executeCommand(
+		"",
+		"rsync",
+		"-av",
+		"--exclude=\".git\"",
+		fmt.Sprintf("\"%s\"", originDir),
+		fmt.Sprintf("\"%s\"", pushDir),
+	)
+
+	return err
+}
+
+func cloneRepo(pullDir string, repoSSH string) error {
+	repoFolder := getRepoFolder(repoSSH, pullDir)
+
+	_, err := os.Stat(repoFolder)
+	if !os.IsNotExist(err) {
+		log.Print("Repository already initialized")
+		return nil
+	}
+
+	log.Printf("Initializing repository %s", repoSSH)
+	err = executeCommand(pullDir, "git", "clone", repoSSH)
+	return err
+}
+
+func getAllBranches(pullDir string, repoSSH string) ([]string, error) {
+	cmd := exec.Command("git", "branch", "-a")
+	cmd.Dir = getRepoFolder(repoSSH, pullDir)
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(output), "\n")
+	var branches []string
+	for _, line := range lines {
+		if strings.HasPrefix(line, "*") {
+			line = strings.TrimPrefix(line, "*")
+		}
+		line = strings.TrimSpace(line)
+		if line != "" {
+			branches = append(branches, line)
+		}
+	}
+
+	return branches, nil
 }
